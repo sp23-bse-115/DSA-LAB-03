137f1196a2d51d6716e8204a2191ca6c287f0a3e
sp23-bse-115 <ak5604178@gmail.com>
2025-03-24 15:16:09 +0500

    text

---
+++ lab3/observer/src/SMSSupportListener.java
@@ -0,0 +1,24 @@
+public class SMSSupportListener {
+    package observer.eventsmanagement;
+
+public class SMSSupportListener {
+    private String phoneNumber;
+
+    public SMSSupportListener(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void update(String message) {
+        if (message.length() > 160) {
+            System.out.println("Warning: SMS exceeds 160 characters. Please shorten the message.");
+        } else {
+            sendSMS(phoneNumber, message);
+        }
+    }
+
+    private void sendSMS(String phoneNumber, String message) {
+        System.out.println("Sending SMS to " + phoneNumber + ": " + message);
+    }
+}
+
+}
